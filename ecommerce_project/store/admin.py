5fe3e5104d95ae2095370442b52a7477e73f29b9
Octavian <octavoctav12@gmail.com>
2024-06-20 21:15:18 +0300

    modified admin interface to show more information (not to click each product to show it's info

--- ecommerce_project/store/admin.py
+++ ecommerce_project/store/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 from .models import Category, Product
+
+
 # Register your models here.
 
-admin.site.register(Category)
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):  # use this to view more info about in the admin page regarding products
+    list_display = ['name', 'price', 'stock', 'available', 'created', 'updated']
+    list_editable = ['price', 'stock', 'available']
+    prepopulated_fields = {'slug': ('name',)}
+    list_per_page = 20
+
+
+class CategoryAdmin(admin.ModelAdmin):  # same as above but for categories
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin,)
